5ac9d72a327aaf8d5883b6704df01360f5f5d0fe
parkchangheon <seanpch@naver.com>
2023-03-13 23:41:49 +0900

    Client <-> Server Connection start
    Client <-> Server Connection start

---
+++ UnrealNetwork/Source/UnrealNetwork/UnrealNetworkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealNetworkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealNetworkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealNetworkGameMode();
+};
+
+
+
